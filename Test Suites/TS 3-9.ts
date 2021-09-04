17ed77b98e99bc95fa9ade974ce99f6c55e26964
thainhu1706 <thainhu176@gmail.com>
2021-09-04 11:23:35 +0700

    Commit 3-9

---
+++ Test Suites/TS 3-9.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS 3-9</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>91c05aed-e4d9-4b7a-a77b-13beb8a027f1</testSuiteGuid>
+   <testCaseLink>
+      <guid>a8c7f33c-d85a-4ede-a3e0-75db3b77477f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Using Record Tool 3-9</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
